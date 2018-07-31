4a8123bdfdac1561d1a21103b7c4c2d614ef707f
181710038 <181710038@LAB30811.senacinformatica.br>
2018-07-31 11:59:27 -0300

    ObjCargo feito

--- src/model/ObjCargo.java
+++ src/model/ObjCargo.java
@@ -2,5 +2,30 @@ package model;
 
 public class ObjCargo {
     private int codigo;
-    private 
+    private String nome;
+    private double salario;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
